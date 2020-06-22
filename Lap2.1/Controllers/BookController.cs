7028b490099d827994e90ae0a14863aebd25b2a2
Huu Loc Truong <huuloctruong138@gmail.com>
2020-06-22 08:20:54 +0700

    first commit

---
+++ Lap2.1/Controllers/BookController.cs
@@ -0,0 +1,118 @@
+﻿using Lap2._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lap2._1.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+       
+        public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("HTML5 & CSS3 The complete Manual-Author Name Book 1");
+            books.Add("HTML5 & CSS Responsive web Design cookbook-Author Name Book 2");
+            books.Add("Professional ASP.NET MVC5 - Author Name Book 2");
+            ViewBag.Books = books;
+            return View();
+        }
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1,"Quản lý dự án Công nghệ thông tin","Nguyễn Văn A","/Content/Images/book1cover.jpg","Edit","Delete"));
+            books.Add(new Book(2,"Sách công nghệ thông tin và truyền thông Việt Nam","Trần Thị B","/Content/Images/book2cover.jpg","Edit","Delete"));
+            books.Add(new Book(3,"Công nghệ thông tin, Lãnh đạo quản lý","Trương Lâm Hữu Lộc","/Content/Images/book3cover.jpg","Edit","Delete"));
+            
+            return View(books);
+        }
+        public string Hello()
+        {
+            return "My_name:Trương Lâm Hữu Lộc";
+        }
+        public string HelloStudent(string HuuLocTruong)
+        {
+            return "My_name:Trương Lâm Hữu Lộc"+ HuuLocTruong;
+        }
+        public ActionResult EditBook(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Quản lý dự án Công nghệ thông tin", "Nguyễn Văn A", "/Content/Images/book1cover.jpg"));
+            books.Add(new Book(2, "Sách công nghệ thông tin và truyền thông Việt Nam", "Trần Thị B", "/Content/Images/book2cover.jpg"));
+            books.Add(new Book(3, "Công nghệ thông tin, Lãnh đạo quản lý", "Trương Lâm Hữu Lộc", "/Content/Images/book3cover.jpg"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+
+        }
+        [HttpPost, ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int id, string Title, string Author, string ImageCover)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Quản lý dự án Công nghệ thông tin", "Nguyễn Văn A", "/Content/Images/book1cover.jpg"));
+            books.Add(new Book(2, "Sách công nghệ thông tin và truyền thông Việt Nam", "Trần Thị B", "/Content/Images/book2cover.jpg"));
+            books.Add(new Book(3, "Công nghệ thông tin, Lãnh đạo quản lý", "Trương Lâm Hữu Lộc", "/Content/Images/book3cover.jpg"));
+           
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.ImageCover = ImageCover;
+                    break;
+                }
+
+            }
+
+            return View("ListBookModel", books);
+
+        }
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+        [HttpPost,ActionName("CreateBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact([Bind(Include ="Id,Title,Author,ImageCover")]Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "Quản lý dự án Công nghệ thông tin", "Nguyễn Văn A", "/Content/Images/book1cover.jpg"));
+            books.Add(new Book(2, "Sách công nghệ thông tin và truyền thông Việt Nam","Trần Thị B", "/Content/Images/book2cover.jpg"));
+            books.Add(new Book(3, "Công nghệ thông tin, Lãnh đạo quản lý", "Trương Lâm Hữu Lộc", "/Content/Images/book3cover.jpg"));
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    //Thêm mới sách
+                    books.Add(book);
+                }    
+            }
+            catch(RetryLimitExceededException/*dex*/)
+            {
+                ModelState.AddModelError("", "Error Save Data");
+            }
+            return View("ListBookModel",books);
+        }
+    }
+}
---
+++ Lap2.1/Models/Book.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lap2._1.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+        private string edit;
+        private string delete;
+        private int v1;
+        private string v2;
+        private string v3;
+        private string v4;
+
+        public Book()
+        {
+
+        }
+
+        public Book(int v1, string v2, string v3, string v4)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+        }
+
+        public Book(int id,string title,string author,string image_cover,string edit,string delete)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = image_cover;
+            this.edit = edit;
+            this.delete = delete;
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        [Required(ErrorMessage ="Tiêu đề không được bỏ trống")]
+        [StringLength(250,ErrorMessage ="Tiêu đề không vượt quá 250 ký tự")]
+        [Display(Name ="Title")]
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ImageCover
+        {
+            get { return image_cover; }
+            set { image_cover = value; }
+        }
+        public string Edit
+        {
+            get { return edit; }
+            set { edit = value; }
+        }
+        public string Delete
+        {
+            get { return delete; }
+            set { delete = value; }
+        }
+     
+
+    }
+}
